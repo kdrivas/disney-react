6394e9690c36a5661066337be1f3a0bd648e88e1
kervyRivas <a20112128@pucp.pe>
2021-09-24 18:43:51 -0500

    adding classes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,57 @@ import styled from 'styled-components';
 const Header = props => {
   return (
     <Nav>
-      header
+      <Logo>
+        <img src="images/logo.svg"></img>
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="images/home-icon.svg"></img>
+        </a>
+      </NavMenu>
     </Nav>
   );
 };
 
 const Nav = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  z-index: 3;
+`;
 
+const Logo = styled.a`
+  width: 80px;
+  padding: 0;
+  font-size: 0;
+  margin-top: 4px;
+  display: inline-block;
+
+  img {
+    display: block;
+    width: 100%auto;
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  padding: 0;
+  margin-right: auto;
+  margin-left: 25px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export default Header;
