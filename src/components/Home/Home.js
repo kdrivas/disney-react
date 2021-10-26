198bb30db416b009c1ce3c67d6252776872aa6ab
kervyRivas <a20112128@pucp.pe>
2021-10-25 22:14:15 -0500

    adding background

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Home = props => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <h2>HELLO</h2>
+      <h2>HELLO</h2>
+      <h2>HELLO</h2>
+      <h2>HELLO</h2>
+      <h2>HELLO</h2>
+      <h2>HELLO</h2>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  position: relative;
+  background: url('/images/home-background.png') center center / cover no-repeat fixed;
+  overflow-x: hidden;
+  min-height: calc(100vh - 72px);
+  padding: 0 calc(3.5vw + 5px);
+  top: 72px;
+`;
 
 export default Home;
